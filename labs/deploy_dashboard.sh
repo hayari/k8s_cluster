7331a8be18e09db6f214bd2d321e97b8031a5c00
Hichem AYARI <hayari@gmail.com>
2021-01-11 12:04:59 +0100

    2nd commit

--- labs/deploy_dashboard.sh
+++ labs/deploy_dashboard.sh
@@ -1,4 +1,5 @@
 kubectl apply -f https://raw.githubusercontent.com/kubernetes/dashboard/master/aio/deploy/recommended.yaml
 kubectl apply -f dashboard-admin.yml
+# get the token
 bash get_token.sh
 kubectl proxy
